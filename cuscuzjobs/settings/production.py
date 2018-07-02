58972cf163174b7585621de9fff4f0480a9e5e69
Matheus Lins <msl@cin.ufpe.br>
2018-07-01 23:20:07 -0300

    Reconfig the production enviroment

--- cuscuzjobs/settings/production.py
+++ cuscuzjobs/settings/production.py
@@ -1,20 +1,10 @@
-import dj_database_url
 import django_heroku
-
-from dj_database_url import parse as db_url
-from decouple import config
 
 from .common import *
 
-# ALLOWED_HOSTS = ['cuscuzjobs.herokuapp.com', 'cuscuzjobs.com.br']
 
-# DATABASES = {
-#     'default': config('DATABASE_URL', cast=db_url)
-# }
+ALLOWED_HOSTS = ['cuscuzjobs.herokuapp.com', 'cuscuzjobs.com.br']
 
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
-
-# DEBUG = False
+DEBUG = False
 
 django_heroku.settings(locals())
