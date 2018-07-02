8c0e4225820d85043d5c7f4b7ff466468a8332da
Matheus Lins <msl@cin.ufpe.br>
2018-07-02 00:04:08 -0300

    .

--- cuscuzjobs/settings/production.py
+++ cuscuzjobs/settings/production.py
@@ -1,10 +1,16 @@
-import django_heroku
+import dj_database_url
+from dj_database_url import parse as db_url
+from decouple import config
 
 from .common import *
 
-
 ALLOWED_HOSTS = ['*']
 
-DEBUG = True
+DATABASES = {
+    'default': config('DATABASE_URL', cast=db_url)
+}
 
-django_heroku.settings(locals())
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+DEBUG = True
