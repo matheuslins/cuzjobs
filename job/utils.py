87e85fc2197b44ad29770a1ede8e2cad9aff3216
Matheus Lins <msl@cin.ufpe.br>
2018-07-05 00:48:24 -0300

    [master] Create views do manage the Job app
    Create, List, Retrieve and Update

---
+++ job/utils.py
@@ -0,0 +1,14 @@
+from company.models import Company
+
+
+def create_object_from_field(data, fields):
+    new_data = []
+    for dt in data:
+        payload = {'name': dt.get(fields[0])}
+        list_update = [{field: dt.get(field)} for field in fields[1]]
+        for dict_field in list_update:
+            payload.update(dict_field)
+        obj, created = Company.objects.get_or_create(**payload)
+        dt[fields[0]] = obj.id
+        new_data.append(dt)
+    return new_data
--- job/views.py
+++ job/views.py
@@ -1,1 +1,36 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import exception_handler
+from rest_framework import generics
 from django.shortcuts import render
+
+from core.mixins import CreateListMixin
+from .serializer import JobSerializer
+from .models import Job
+from .utils import create_object_from_field
+
+
+class CreateJobAPI(CreateListMixin, generics.CreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        fields = ('company', ['company_size', 'company_type', 'industry'])
+        serializer.initial_data = create_object_from_field(
+            serializer.initial_data, fields)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+class ListJobAPI(generics.ListAPIView):
+    serializer_class = JobSerializer
+    queryset = Job.objects.all()
+
+
+class RetrieveUpdateJobAPI(generics.RetrieveUpdateAPIView):
+    serializer_class = JobSerializer
+    queryset = Job.objects.all()
+    lookup_field = 'job_id'
