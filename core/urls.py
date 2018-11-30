f167b0cd0abecfa2ac7caffceda69f9c527f6558
Matheus Lins <matheus.lins@br.experian.com>
2018-11-29 23:13:35 -0200

    [social-login] urls

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,6 @@ from .views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('login/', auth_views.login, {'template_name': 'login.html'},
-        name='login'),
-    path('sair', auth_views.logout, {'next_page': 'core:login'}, name='logout')
+    path('login', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    path('logout', auth_views.logout, {'next_page': 'core:login'}, name='logout')
 ]
