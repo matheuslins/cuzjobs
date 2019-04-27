4636fe001e04f03fb6c24102b9300fb15feb98b9
Matheus Lins <matheus.lins@br.experian.com>
2019-04-27 18:32:30 -0300

    estabilizacao

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 
 from .views import HomeView
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('login', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    path('logout', auth_views.logout, {'next_page': 'core:login'}, name='logout')
 ]
--- job/views.py
+++ job/views.py
@@ -1,8 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import exception_handler
 from rest_framework import generics
-from django.shortcuts import render
 
 from core.mixins import CreateListMixin
 from .serializer import JobSerializer
@@ -19,13 +17,13 @@ class CreateJobAPI(CreateListMixin, generics.CreateAPIView):
         fields = ('company', ['company_size', 'company_type', 'industry'])
         serializer.initial_data = create_object_from_field(serializer.initial_data, fields)
         serializer.is_valid(raise_exception=True)
+
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 class ListJobAPI(generics.ListAPIView):
     serializer_class = JobSerializer
-    queryset = Job.objects.all()
 
 
 class RetrieveUpdateJobAPI(generics.RetrieveUpdateAPIView):
