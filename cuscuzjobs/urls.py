f499c351cf13c7b7d0bb5e00879576244db03869
Matheus Lins <matheuslins9@gmail.com>
2020-03-29 01:30:42 -0300

    Overriden login url

--- cuscuzjobs/urls.py
+++ cuscuzjobs/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from allauth.account import views
 
 
 urlpatterns = [
     path('', include('allauth.urls')),
+    path("", views.login, name="account_login"),
     path('auth/', include(('users_auth.urls', 'users_auth'), namespace='auth')),
     path('job/', include(('job.urls', 'users_auth'), namespace='job')),
     path('admin/', admin.site.urls)
