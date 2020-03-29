4dba3f75922d16c4776120a84a97c3557ff35344
Matheus Lins <matheuslins9@gmail.com>
2020-03-29 18:54:00 -0300

    Fix field in job models name

--- job/urls.py
+++ job/urls.py
@@ -4,20 +4,24 @@ from .views import CreateJobAPI, AllJobsHandler, RetrieveUpdateJobAPI, JobsNextT
 
 
 urlpatterns = [
-    path('<int:job_id>',
-         RetrieveUpdateJobAPI.as_view(),
-         name='retrieve_update'
+    path(
+        '<int:job_id>',
+        RetrieveUpdateJobAPI.as_view(),
+        name='retrieve_update'
     ),
-    path('create/',
-         CreateJobAPI.as_view(),
-         name='create'
+    path(
+        'create/',
+        CreateJobAPI.as_view(),
+        name='create'
     ),
-    path('all-jobs/',
-         AllJobsHandler.as_view(),
-         name='all_jobs'
+    path(
+        'all-jobs/',
+        AllJobsHandler.as_view(),
+        name='all_jobs'
     ),
-    path('next-to-you/',
-         JobsNextToYouHandler.as_view(),
-         name='next_to_you'
+    path(
+        'next-to-you/',
+        JobsNextToYouHandler.as_view(),
+        name='next_to_you'
     )
 ]
