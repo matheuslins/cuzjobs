54a634a8f01f7fe9bd831e6e1304b07cccce0ca1
Matheus Lins <matheuslins9@gmail.com>
2020-03-29 01:31:10 -0300

    Update readme

--- cuscuzjobs/urls.py
+++ cuscuzjobs/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from allauth.account import views
+from allauth.account.views import login
 
 
 urlpatterns = [
     path('', include('allauth.urls')),
-    path("", views.login, name="account_login"),
+    path("", login, name="account_login"),
     path('auth/', include(('users_auth.urls', 'users_auth'), namespace='auth')),
     path('job/', include(('job.urls', 'users_auth'), namespace='job')),
     path('admin/', admin.site.urls)
