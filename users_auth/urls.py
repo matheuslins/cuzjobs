a9866536d3f04321fd7850a9ff24326b70186718
Matheus Lins <matheuslins9@gmail.com>
2020-04-24 18:31:41 -0300

    Apply login required in routes

--- users_auth/urls.py
+++ users_auth/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
+from django.contrib.auth.decorators import login_required
+
 from .views import DashboardView, ProfileReport, Profile
 
 
 urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard'),
-    path('profile', Profile.as_view(), name='profile'),
-    path('profile-report', ProfileReport.as_view(), name='profile_report')
+    path('', login_required(DashboardView.as_view()), name='dashboard'),
+    path('profile', login_required(Profile.as_view()), name='profile'),
+    path('profile-report', login_required(ProfileReport.as_view()), name='profile_report')
 ]
