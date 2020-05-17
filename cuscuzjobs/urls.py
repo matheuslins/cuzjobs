b9bb3cfd9b3bc37eaff1193d351d8f8f10cc6f22
Matheus Lins <matheuslins9@gmail.com>
2020-05-17 14:57:27 -0300

    Remove home icon

--- cuscuzjobs/urls.py
+++ cuscuzjobs/urls.py
@@ -8,7 +8,7 @@ from cuscuzjobs.settings.common import MEDIA_ROOT, MEDIA_URL
 urlpatterns = [
     path('', include('allauth.urls')),
     path('', login, name="account_login"),
-    path('auth/', include(('apps.users_auth.urls', 'apps.users_auth'), namespace='auth')),
+    path('dashboard/', include(('apps.users_auth.urls', 'apps.users_auth'), namespace='auth')),
     path('job/', include(('apps.job.urls', 'apps.users_auth'), namespace='job')),
     path('languages/', include(('apps.language.urls', 'apps.language'), namespace="language")),
     path('admin/', admin.site.urls)
